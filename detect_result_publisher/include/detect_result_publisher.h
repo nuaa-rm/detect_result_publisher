4a2cdac8043867a5202f8fef6e407718bc743af3
zhu <z3094267060@163.com>
2024-05-23 13:17:18 +0800

    方向已经添加

--- detect_result_publisher/include/detect_result_publisher.h
+++ detect_result_publisher/include/detect_result_publisher.h
@@ -5,6 +5,7 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "detector_interfaces/msg/detect_result.hpp"
 #include "random"
+#include "cmath"
 
 using namespace std::chrono_literals;
 
@@ -15,16 +16,21 @@ public:
 
 
 private:
+    double v[3]={0},omiga=0;
+    double posture[4]={0};
+    double fps=200,T=0.3;
     using DetectResult = detector_interfaces::msg::DetectResult;
     rclcpp::Publisher<DetectResult>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_2;
     DetectResult message = DetectResult();
     geometry_msgs::msg::PoseStamped m = geometry_msgs::msg::PoseStamped();
-    double v[3]={0};
+
 
     void timer_callback();
     void loca_make(geometry_msgs::msg::PoseStamped &pose,double *v);
+    void posture_make(geometry_msgs::msg::PoseStamped &pose,double *posture);
+    void eulerToQuaternion(double roll, double pitch, double yaw,double* posture);
 
 };
 
--- detect_result_publisher/src/detect_result_publisher.cpp
+++ detect_result_publisher/src/detect_result_publisher.cpp
@@ -6,7 +6,7 @@ DetectResultPublisher::DetectResultPublisher()
     publisher_ = this->create_publisher<DetectResult>("DetectResult",10);
     publisher_2 = this->create_publisher<geometry_msgs::msg::PoseStamped>("test",1);
     timer_  =  this->create_wall_timer(
-        5ms, std::bind(&DetectResultPublisher::timer_callback, this));
+        50ms, std::bind(&DetectResultPublisher::timer_callback, this));
 }
 
 void DetectResultPublisher::timer_callback()
@@ -18,6 +18,7 @@ void DetectResultPublisher::timer_callback()
     message.pose.header.stamp = this->get_clock()->now();
 
     loca_make(message.pose,v);
+    posture_make(message.pose,posture);
 
     publisher_->publish(message);
     publisher_2 ->publish(message.pose);
@@ -31,18 +32,60 @@ void DetectResultPublisher::loca_make(geometry_msgs::msg::PoseStamped &pose,doub
 
     double a[3]={dist(generator),dist(generator),dist(generator)};
     
-    v[0]+=a[0]*0.03;
-    v[1]+=a[1]*0.03;
-    v[2]+=a[2]*0.03;
+    v[0]+=a[0]*T;
+    v[1]+=a[1]*T;
+    v[2]+=a[2]*T;
 
-    pose.pose.position.x+=v[0]*0.03;
-    pose.pose.position.y+=v[1]*0.03;
-    pose.pose.position.z+=v[2]*0.03;
+    pose.pose.position.x+=v[0]*T;
+    pose.pose.position.y+=v[1]*T;
+    pose.pose.position.z+=v[2]*T;
 
     if(pose.pose.position.x<-10||pose.pose.position.x>10) v[0]*=-1;
     if(pose.pose.position.y<-10||pose.pose.position.y>10) v[1]*=-1;
     if(pose.pose.position.z<0||pose.pose.position.z>10) v[2]*=-1;
 
 }
+void DetectResultPublisher::posture_make(geometry_msgs::msg::PoseStamped &pose,double *posture)
+{
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::normal_distribution<double> dist(0,50);
+    double a=dist(generator);
+    omiga+=a*T;
+    eulerToQuaternion(omiga*T,0,0,posture);
+    pose.pose.orientation.w=posture[0];
+    pose.pose.orientation.x=posture[1];
+    pose.pose.orientation.y=posture[2];
+    pose.pose.orientation.z=posture[3];
+}
+void DetectResultPublisher::eulerToQuaternion(double roll, double pitch, double yaw,double *posture) {
+    // 将角度转换为弧度
+    roll = roll * M_PI / 180.0f;
+    pitch = pitch * M_PI / 180.0f;
+    yaw = yaw * M_PI / 180.0f;
 
+    double cX = cos(yaw * 0.5f);
+    double sX = sin(yaw * 0.5f);
+    double cY = cos(pitch * 0.5f);
+    double sY = sin(pitch * 0.5f);
+    double cZ = cos(roll * 0.5f);
+    double sZ = sin(roll * 0.5f);
+
+    // 构造四元数
+    double w = cX * cY * cZ + sX * sY * sZ;
+    double x = sX * cY * cZ - cX * sY * sZ;
+    double y = cX * sY * cZ + sX * cY * sZ;
+    double z = cX * cY * sZ - sX * sY * cZ;
+    // 归一化四元数
+    double norm = sqrt(w*w + x*x + y*y + z*z);
+    w /= norm;
+    x /= norm;
+    y /= norm;
+    z /= norm;
+
+    posture[0]=w;
+    posture[1]=x;
+    posture[2]=y;
+    posture[3]=z;
+}
 
